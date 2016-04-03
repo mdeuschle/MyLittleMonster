01c7ee807de2db0cbf3d2a9a64b17eac14de39b8
Matt Deuschle <mdeuschle@gmail.com>
2016-04-03 13:00:51 -0700

    added breathing animation

--- MyLittleMonster/ViewController.swift
+++ MyLittleMonster/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monsterImg: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let monsterImageArray: [UIImage] = [(UIImage(named: "idle1")!),
+                                            (UIImage(named: "idle2")!),
+                                            (UIImage(named: "idle3")!),
+                                            (UIImage(named: "idle4")!)]
+
+        monsterImg.animationImages = monsterImageArray
+        monsterImg.animationDuration = 0.8
+        monsterImg.animationRepeatCount = 0
+        monsterImg.startAnimating()
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
